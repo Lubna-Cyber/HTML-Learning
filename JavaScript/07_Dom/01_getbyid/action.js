0d15ee1980de35eb9531e5b29e793ce9eefcb2e6
Lubna-Cyber <rajputlubna30@gmail.com>
2025-03-17 22:36:36 +0530

    DOM

---
+++ JavaScript/07_Dom/01_getbyid/action.js
@@ -0,0 +1,20 @@
+// DOM Document Object Model
+// Reading
+// Updating
+// Create
+// Delete
+
+function getText()
+{
+
+    // let head1 = document.getElementById("head1")
+    
+    // let text = head1.innerText
+    
+    // head1.innerText = "Go Away"
+    // console.log(text);
+    
+    
+    // console.log(head1);
+}
+
