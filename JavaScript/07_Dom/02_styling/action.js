3754f04e696d3a8287ab59cb83b847b806f759a1
Lubna-Cyber <rajputlubna30@gmail.com>
2025-03-19 10:36:11 +0530

    styling

---
+++ JavaScript/07_Dom/02_styling/action.js
@@ -0,0 +1,31 @@
+// function changeColor()
+// {
+//     let text = document.getElementById("text")
+//     text.style.color="red"
+// }
+
+function Change()
+{
+    let text=document.getElementById("text")
+    text.style.color="blue";
+}
+
+function fontItalic()
+{
+    let textarea=document.getElementById("text");
+    textarea.style.fontStyle="italic" ;
+    textarea.style.textTransform="uppercase" ;
+
+}
+
+function styleBold()
+{
+    let textarea=document.getElementById("text");
+    textarea.style.fontWeight="bold";
+}
+
+function styleUnderline()
+{
+    let textarea=document.getElementById("text");
+    textarea.style.textDecoration="Underline";
+}
